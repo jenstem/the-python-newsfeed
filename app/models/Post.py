3c16f47593c690a9156f23a1693216fe8a7978e1
Jen <jennifer.stemkowski@gmail.com>
2024-02-27 16:09:27 -0500

    Add Vote model

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 # Post model
 class Post(Base):
@@ -12,8 +13,13 @@ class Post(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+  vote_count = column_property(
+  select(func.count(Vote.id)).where(Vote.post_id == id)
+)
 
   # user relationship to User model
   user = relationship('User')
   # comments relationship to Comment model
   comments = relationship('Comment', cascade='all,delete')
+  # votes relationship to Vote model
+  votes = relationship('Vote', cascade='all,delete')

f9c371f57b8cbaa8519b2423197320989e14a799
Jen <124745888+jenstem@users.noreply.github.com>
2024-11-10 18:25:40 -0500

    Add docstring

--- app/models/Vote.py
+++ app/models/Vote.py
@@ -2,6 +2,14 @@ from app.db import Base
 from sqlalchemy import Column, Integer, ForeignKey
 
 class Vote(Base):
+  """
+  Represents a vote in the voting system.
+
+  Attributes:
+    id (int): The unique identifier for the vote.
+    user_id (int): The identifier of the user cast the vote.
+    post_id (int): The identified of the post that received the vote.
+  """
   __tablename__ = 'votes'
   id = Column(Integer, primary_key=True)
   user_id = Column(Integer, ForeignKey('users.id'))

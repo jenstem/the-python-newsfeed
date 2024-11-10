e078a306b03e8700fd9a3ac5de1629ace8c5c815
Jen <124745888+jenstem@users.noreply.github.com>
2024-11-10 18:23:22 -0500

    Add docstring

--- app/routes/home.py
+++ app/routes/home.py
@@ -6,7 +6,12 @@ bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-  # get all posts
+  """
+  Render the homepage with all posts.
+
+  Retrieves all posts from the database, orders them by creation date,
+  and renders the homepage template with the posts and login status.
+  """
   db = get_db()
   posts = db.query(Post).order_by(Post.created_at.desc()).all()
 
@@ -18,7 +23,12 @@ def index():
 
 @bp.route('/login')
 def login():
-  # if user is already logged in, redirect to home
+  """
+  Render the login page or redirect to the dashboard.
+
+  Checks if the user is already logged in.  If not, it renders the 
+  login template, If the user is logged in, it redirects to the dashboard.
+  """
   if session.get('loggedIn') is None:
     return render_template('login.html')
 
@@ -26,7 +36,13 @@ def login():
 
 @bp.route('/post/<id>')
 def single(id):
-  # get single post by id
+  """
+  Render a single post based on its ID.
+
+  Retrieves a specific post from the database using the provided ID
+  and renders the single post template with the post details and
+  login status.
+  """
   db = get_db()
   post = db.query(Post).filter(Post.id == id).one()
 

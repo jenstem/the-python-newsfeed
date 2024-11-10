2033a18bd2540875d06cf9027d1c9a7010fa2cb5
Jen <124745888+jenstem@users.noreply.github.com>
2024-11-10 18:20:59 -0500

    Add docstring

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -8,6 +8,16 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 @bp.route('/')
 @login_required
 def dash():
+  """
+  Render the dashboard with the user's posts.
+
+  This function queries the database for all posts created by the logged-in user,
+  orders them by creation date in descending order, and renders the dashboard template
+  with the retrieved posts.
+
+  Returns:
+    Rendered HTML template for the dashboard.
+  """
   db = get_db()
   posts = (
       db.query(Post)
@@ -24,6 +34,18 @@ def dash():
 @bp.route('/edit/<id>')
 @login_required
 def edit(id):
+  """
+  Render the edit page for a specific post.
+
+  This function retrieves a single post from the database using its ID and renders
+  the edit post template with the post data.
+
+  Args:
+    id (int): The Id of the post to be edited.
+
+  Returns:
+    Rendered HTML template for editing the post.
+  """
   # get single post by id
   db = get_db()
   post = db.query(Post).filter(Post.id == id).one()
